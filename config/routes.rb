0622c0434efdac1208d6823d8015229f9fe148b6
m20ja1 <264754617+m20ja1@users.noreply.github.com>
2026-03-10 06:29:06 +0000

    テスト完了

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   get 'bookers' => 'books#index'
 
   resources :books
-
-  resources :books
   get "homes/top"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
